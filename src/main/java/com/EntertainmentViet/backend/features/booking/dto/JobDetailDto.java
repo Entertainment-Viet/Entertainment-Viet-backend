705ca2527ee16f7458adff2ea27a1d44be9aa211
duckhai1 <huynhduc.khai0@gmail.com>
2022-08-08 21:23:42 +0700

    Refactoring

---
+++ src/main/java/com/EntertainmentViet/backend/features/booking/dto/JobDetailDto.java
@@ -0,0 +1,6 @@
+package com.EntertainmentViet.backend.features.booking.dto;
+
+public class JobDetailDto {
+
+  private CategoryDto categoryDto;
+}
--- src/main/java/com/EntertainmentViet/backend/features/organizer/dto/OrganizerMapper.java
+++ src/main/java/com/EntertainmentViet/backend/features/organizer/dto/OrganizerMapper.java
@@ -1,17 +1,37 @@
 package com.EntertainmentViet.backend.features.organizer.dto;
 
 import com.EntertainmentViet.backend.config.MappingConfig;
-import com.EntertainmentViet.backend.domain.entities.Organizer;
+import com.EntertainmentViet.backend.domain.entities.organizer.Organizer;
+import com.EntertainmentViet.backend.domain.standardTypes.UserState;
 import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 @Mapper(config = MappingConfig.class)
 public abstract class OrganizerMapper {
 
-  @BeanMapping(ignoreUnmappedSourceProperties = {"id"})
+  @BeanMapping(ignoreUnmappedSourceProperties = {"id", "jobOffers", "events", "bookings", "feedbacks"}) // TODO: enable jobOffers source mapping
+  @Mapping(target = "jobOfferDto", ignore = true) // TODO: remove and fix this
+  @Mapping(target = "userState", source = "userState", qualifiedByName = "toUserStateKey")
   public abstract OrganizerDto toDto(Organizer organizer);
 
+  @BeanMapping(ignoreUnmappedSourceProperties = {"jobOfferDto"}) // TODO: remove and fix this
   @Mapping(target = "id", ignore = true)
+  @Mapping(target = "userState", source = "userState", qualifiedByName = "toUserState")
+  @Mapping(target = "jobOffers", ignore = true) // TODO: remove and fix this
+  @Mapping(target = "events", ignore = true) // TODO: remove and fix this
+  @Mapping(target = "bookings", ignore = true) // TODO: remove and fix this
+  @Mapping(target = "feedbacks", ignore = true) // TODO: remove and fix this
   public abstract Organizer toModel(OrganizerDto organizerDto);
+
+  @Named("toUserStateKey")
+  public String toUserStateKey(UserState userState) {
+    return userState != null ? userState.i18nKey : null;
+  }
+
+  @Named("toUserState")
+  public UserState toUserState(String i18nKey) {
+    return UserState.ofI18nKey(i18nKey);
+  }
 }
