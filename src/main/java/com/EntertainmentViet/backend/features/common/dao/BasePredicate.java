557f1e71876a8f60b7cc65ee6bf910272fcd9682
duckhai1 <huynhduc.khai0@gmail.com>
2022-08-17 00:37:39 +0700

    Bug fix on compiling base predicate

--- src/main/java/com/EntertainmentViet/backend/features/common/dao/BasePredicate.java
+++ src/main/java/com/EntertainmentViet/backend/features/common/dao/BasePredicate.java
@@ -1,9 +1,12 @@
 package com.EntertainmentViet.backend.features.common.dao;
 
-import com.EntertainmentViet.backend.domain.entities.organizer.JobOffer;
 import com.EntertainmentViet.backend.features.common.JoinExpression;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 
 public abstract class BasePredicate<T> {
 
-  public abstract JoinExpression<JobOffer> joinAll();
+  public abstract JoinExpression joinAll();
+
+  public abstract JPAQuery<T> getRootBase(JPAQueryFactory queryFactory);
 }
