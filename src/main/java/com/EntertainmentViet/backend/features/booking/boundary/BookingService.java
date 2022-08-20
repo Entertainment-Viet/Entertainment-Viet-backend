280bfe8f23a60be92dff84f308e1f4704910d51e
duckhai1 <huynhduc.khai0@gmail.com>
2022-08-21 01:43:19 +0700

    Fix hibernate error

--- src/main/java/com/EntertainmentViet/backend/features/booking/boundary/BookingService.java
+++ src/main/java/com/EntertainmentViet/backend/features/booking/boundary/BookingService.java
@@ -4,7 +4,9 @@ import com.EntertainmentViet.backend.domain.entities.booking.Booking;
 import com.EntertainmentViet.backend.domain.entities.booking.JobDetail;
 import com.EntertainmentViet.backend.domain.entities.organizer.Organizer;
 import com.EntertainmentViet.backend.domain.entities.talent.Talent;
+import com.EntertainmentViet.backend.domain.values.Category;
 import com.EntertainmentViet.backend.features.booking.dao.BookingRepository;
+import com.EntertainmentViet.backend.features.booking.dao.CategoryRepository;
 import com.EntertainmentViet.backend.features.booking.dto.BookingDto;
 import com.EntertainmentViet.backend.features.booking.dto.BookingMapper;
 import com.EntertainmentViet.backend.features.booking.dto.JobDetailDto;
@@ -13,6 +15,7 @@ import com.EntertainmentViet.backend.features.organizer.dao.OrganizerRepository;
 import com.EntertainmentViet.backend.features.talent.dao.TalentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
@@ -32,19 +35,21 @@ public class BookingService implements BookingBoundary {
 
     private final JobDetailMapper jobDetailMapper;
 
+    private final CategoryRepository categoryRepository;
+
     @Override
     public BookingDto findByUid(UUID uid) {
         return bookingMapper.toDto(bookingRepository.findByUid(uid).orElse(null));
     }
 
     @Override
+    @Transactional
     public BookingDto create(BookingDto bookingDto) {
-        Organizer organizer = organizerRepository.findByUid(bookingDto.getOrganizerUid()).get();
-        Talent talent = talentRepository.findByUid(bookingDto.getTalentUid()).get();
-
-        JobDetailDto jobDetailDto = bookingDto.getJobDetailDto();
-
-        JobDetail jobDetail = jobDetailRepository.save(jobDetailMapper.toModel(jobDetailDto));
+        Organizer organizer = organizerRepository.findByUid(bookingDto.getOrganizerUid()).orElse(null);
+        Talent talent = talentRepository.findByUid(bookingDto.getTalentUid()).orElse(null);
+        JobDetail jobDetail = jobDetailMapper.toModel(bookingDto.getJobDetailDto());
+        Category category = categoryRepository.findByName(jobDetail.getCategory().getName()).orElse(null);
+        jobDetail.setCategory(category);
 
         Booking booking = bookingMapper.toModel(bookingDto);
         booking.setOrganizer(organizer);
