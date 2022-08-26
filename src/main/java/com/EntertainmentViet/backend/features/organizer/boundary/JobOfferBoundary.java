4d9a516dddeebdc51e92b67d0d5e21e882e4efdb
Hồ Huy Hoàng <48915815+hohuyhoangg@users.noreply.github.com>
2022-08-27 01:39:13 +0700

    post put delete

--- src/main/java/com/EntertainmentViet/backend/features/organizer/boundary/JobOfferBoundary.java
+++ src/main/java/com/EntertainmentViet/backend/features/organizer/boundary/JobOfferBoundary.java
@@ -11,4 +11,10 @@ public interface JobOfferBoundary {
     List<JobOfferDto> findByOrganizerUid(UUID uid);
 
     Optional<JobOfferDto> findByOrganizerUidAndUid(UUID organizerUid, UUID uid);
+
+    Optional<UUID> create(JobOfferDto jobOfferDto, UUID organizerUid);
+
+    Optional<UUID> update(JobOfferDto jobOfferDto, UUID organizerUid, UUID uid);
+
+    void delete(UUID uid, UUID organizerUid);
 }
