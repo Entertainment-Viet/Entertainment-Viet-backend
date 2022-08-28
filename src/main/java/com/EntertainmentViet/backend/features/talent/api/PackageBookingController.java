353d477824893a03bda1c8b4fd970d3d0b8c1072
Hồ Huy Hoàng <48915815+hohuyhoangg@users.noreply.github.com>
2022-08-28 10:33:22 +0700

    add api

--- src/main/java/com/EntertainmentViet/backend/features/talent/api/PackageBookingController.java
+++ src/main/java/com/EntertainmentViet/backend/features/talent/api/PackageBookingController.java
@@ -1,8 +1,22 @@
 package com.EntertainmentViet.backend.features.talent.api;
 
+import com.EntertainmentViet.backend.features.booking.dto.BookingDto;
+import com.EntertainmentViet.backend.features.common.utils.RestUtils;
+import com.EntertainmentViet.backend.features.talent.boundary.PackageBookingBoundary;
+import com.EntertainmentViet.backend.features.talent.dto.PackageBookingDto;
+import com.EntertainmentViet.backend.features.talent.dto.PackageDto;
+import com.EntertainmentViet.backend.features.talent.dto.TalentDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping(path = PackageBookingController.REQUEST_MAPPING_PATH)
@@ -10,7 +24,52 @@ import org.springframework.web.bind.annotation.RestController;
 public class PackageBookingController {
 
   public static final String REQUEST_MAPPING_PATH = "/talents/{talent_uid}/packages/{package_uid}/bookings";
+  private final PackageBookingBoundary packageBookingService;
 
+  @GetMapping()
+  public CompletableFuture<List<BookingDto>> listBooking(@PathVariable("talent_uid") UUID talentId,
+                                                         @PathVariable("package_uid") UUID packageId) {
+    return CompletableFuture.completedFuture(packageBookingService.listBooking(talentId, packageId));
+  }
 
+  @PostMapping(
+          consumes = MediaType.APPLICATION_JSON_VALUE,
+          produces = MediaType.APPLICATION_JSON_VALUE)
+  @ResponseStatus(HttpStatus.OK)
+  public CompletableFuture<Boolean> acceptBooking(@RequestBody @Valid PackageBookingDto packageBookingDto,
+          @PathVariable("talent_uid") UUID talentId,
+          @PathVariable("package_uid") UUID packageId) {
+    return  CompletableFuture.completedFuture(packageBookingService.addPackageToShoppingCart(talentId, packageId, packageBookingDto));
+  }
+
+  @PostMapping(value = "/{uid}")
+  @ResponseStatus(HttpStatus.OK)
+  public CompletableFuture<ResponseEntity<UUID>> acceptBooking(
+          @PathVariable("talent_uid") UUID talentId,
+          @PathVariable("package_uid") UUID packageId,
+          @PathVariable("uid") UUID bookingId) {
+    return  CompletableFuture.completedFuture(packageBookingService.acceptBooking(talentId, packageId, bookingId)
+            .map(newBookingInfo -> ResponseEntity
+                    .ok()
+                    .body(newBookingInfo)
+            )
+            .orElse(ResponseEntity.badRequest().build())
+    );
+  }
+
+  @PutMapping(value = "/{uid}")
+  @ResponseStatus(HttpStatus.OK)
+  public CompletableFuture<ResponseEntity<UUID>> rejectBooking(
+          @PathVariable("talent_uid") UUID talentId,
+          @PathVariable("package_uid") UUID packageId,
+          @PathVariable("uid") UUID bookingId) {
+    return  CompletableFuture.completedFuture(packageBookingService.rejectBooking(talentId, packageId, bookingId)
+            .map(newBookingInfo -> ResponseEntity
+                    .ok()
+                    .body(newBookingInfo)
+            )
+            .orElse(ResponseEntity.badRequest().build())
+    );
+  }
 
 }
---
+++ src/main/java/com/EntertainmentViet/backend/features/talent/boundary/PackageBookingService.java
@@ -0,0 +1,79 @@
+package com.EntertainmentViet.backend.features.talent.boundary;
+
+import com.EntertainmentViet.backend.domain.entities.organizer.Organizer;
+import com.EntertainmentViet.backend.domain.entities.talent.Package;
+import com.EntertainmentViet.backend.domain.entities.talent.Talent;
+import com.EntertainmentViet.backend.features.booking.dto.BookingDto;
+import com.EntertainmentViet.backend.features.organizer.dao.OrganizerRepository;
+import com.EntertainmentViet.backend.features.talent.dao.PackageRepository;
+import com.EntertainmentViet.backend.features.talent.dao.TalentRepository;
+import com.EntertainmentViet.backend.features.talent.dto.PackageBookingDto;
+import com.EntertainmentViet.backend.features.talent.dto.PackageMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class PackageBookingService implements PackageBookingBoundary {
+
+    private final PackageRepository packageRepository;
+
+    private final PackageMapper packageMapper;
+
+    private final TalentRepository talentRepository;
+
+    private final OrganizerRepository organizerRepository;
+
+
+    @Override
+    public Boolean addPackageToShoppingCart(UUID talentId, UUID packageId, PackageBookingDto packageBookingDto) {
+        Package packageTalent = packageRepository.findByUid(packageBookingDto.getPackageId()).orElse(null);
+        Organizer organizer = organizerRepository.findByUid(packageBookingDto.getOrganizerId()).orElse(null);
+        Talent talent = talentRepository.findByUid(talentId).orElse(null);
+        if (packageTalent != null && organizer != null && talent != null && packageTalent.getTalent().getId() == talent.getId()) {
+            organizer.addPackageToCart(packageTalent);
+            organizerRepository.save(organizer);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public List<BookingDto> listBooking(UUID talentId, UUID packageId) {
+        Talent talent = talentRepository.findByUid(talentId).orElse(null);
+        Package packageTalent = packageRepository.findByUid(packageId).orElse(null);
+        if (talent != null && packageTalent != null && packageTalent.getTalent().getId() == talent.getId()) {
+            return packageRepository.findByUid(packageId).map(packageMapper::toDto).orElse(null).getOrders();
+        }
+        return new ArrayList<>();
+    }
+
+    @Override
+    public Optional<UUID> acceptBooking(UUID talentId, UUID packageId, UUID bookingId) {
+        Talent talent = talentRepository.findByUid(talentId).orElse(null);
+        Package packageTalent = packageRepository.findByUid(packageId).orElse(null);
+        if (talent != null && packageTalent != null && packageTalent.getTalent().getId() == talent.getId()) {
+            talent.acceptBooking(bookingId);
+            talentRepository.save(talent);
+            return Optional.ofNullable(bookingId);
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<UUID> rejectBooking(UUID talentId, UUID packageId, UUID bookingId) {
+        Talent talent = talentRepository.findByUid(talentId).orElse(null);
+        Package packageTalent = packageRepository.findByUid(packageId).orElse(null);
+        if (talent != null && packageTalent != null && packageTalent.getTalent().getId() == talent.getId()) {
+            talent.rejectBooking(bookingId);
+            talentRepository.save(talent);
+            return Optional.ofNullable(bookingId);
+        }
+        return Optional.empty();
+    }
+}
