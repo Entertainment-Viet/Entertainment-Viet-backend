cecfb66827ed7311ccfbc47688b8e870c6d942df
duckhai1 <huynhduc.khai0@gmail..com>
2022-11-06 18:47:44 +0700

    Split score system logic

--- src/main/java/com/EntertainmentViet/backend/domain/entities/talent/Talent.java
+++ src/main/java/com/EntertainmentViet/backend/domain/entities/talent/Talent.java
@@ -1,5 +1,6 @@
 package com.EntertainmentViet.backend.domain.entities.talent;
 
+import com.EntertainmentViet.backend.domain.businessLogic.ScoreSystemUtils;
 import com.EntertainmentViet.backend.domain.entities.User;
 import com.EntertainmentViet.backend.domain.entities.admin.TalentFeedback;
 import com.EntertainmentViet.backend.domain.entities.admin.TalentFeedback_;
@@ -233,89 +234,7 @@ public class Talent extends User implements Advertisable {
       setPriorityScores(new ArrayList<>());
     }
 
-    // Setup existing Score storage
-    Map<String, PriorityScore> scoreAchievementToExistSongs = new HashMap<>();
-    Map<Long, PriorityScore> scoreTypeIdToExistRewards = new HashMap<>();
-    for (PriorityScore curScore : priorityScores) {
-        if (curScore.checkIfSongScore()) {
-          scoreAchievementToExistSongs.put(curScore.getAchievement(), curScore);
-        } else {
-          scoreTypeIdToExistRewards.put(curScore.getScoreType().getId(), curScore);
-        }
-    }
-
-    List<String> finalSongId = new ArrayList<>();
-    List<Long> finalRewardScoreTypeId = new ArrayList<>();
-    newScores.forEach(score -> {
-      // Update Song List
-      if (score.checkIfSongScore()) {
-        var existSong = scoreAchievementToExistSongs.get(score.getAchievement());
-        // Song didn't exist yet
-        if (existSong == null) {
-          var newSongScore = PriorityScore.builder()
-              .talent(this)
-              .scoreType(score.getScoreType())
-              .proof(score.getProof())
-              .achievement(score.getAchievement())
-              .approved(isAdmin ? score.getApproved() : false)
-              .build();
-
-          // Add new Song to current Score List to check if there is duplicated Song, it will override the existing one.
-          scoreAchievementToExistSongs.put(score.getAchievement(), newSongScore);
-          // Add new Song to the final Score list
-          finalSongId.add(score.getAchievement());
-          addScore(newSongScore);
-        }
-        // If Song already exist
-        else {
-          existSong.setAchievement(score.getAchievement());
-          existSong.setProof(score.getProof());
-          existSong.setApproved(isAdmin ? score.getApproved() : false);
-          finalSongId.add(score.getAchievement());
-        }
-      }
-      // Update for Reward List
-      else {
-        if (score.getScoreType() == null) {
-          log.error(String.format("Can not find score type for achievement %s of talent with uid '%s'", score.getAchievement(), getUid()));
-          return;
-        }
-        var existReward = scoreTypeIdToExistRewards.get(score.getScoreType().getId());
-        // If Reward didn't exist yet
-        if (existReward == null) {
-          var newRewardScore = PriorityScore.builder()
-              .talent(this)
-              .scoreType(score.getScoreType())
-              .proof(score.getProof())
-              .achievement(score.getAchievement())
-              .approved(isAdmin ? score.getApproved() : false)
-              .build();
-
-          // Add new Reward to current Score List to check if there is duplicated Reward, it will override the existing one.
-          scoreTypeIdToExistRewards.put(score.getScoreType().getId(), newRewardScore);
-          // Add new Reward to the current Score list
-          finalRewardScoreTypeId.add(score.getScoreType().getId());
-          addScore(newRewardScore);
-        }
-        // If Reward already exist
-        else {
-          existReward.setAchievement(score.getAchievement());
-          existReward.setProof(score.getProof());
-          existReward.setApproved(isAdmin ? score.getApproved() : false);
-          finalRewardScoreTypeId.add(score.getScoreType().getId());
-        }
-      }
-
-    });
-    // Remove unspecified Score
-    scoreAchievementToExistSongs.entrySet().stream()
-        .filter(entry -> !finalSongId.contains(entry.getKey()))
-        .forEach(entry -> removeScore(entry.getValue()));
-
-    scoreTypeIdToExistRewards.entrySet().stream()
-        .filter(entry -> !finalRewardScoreTypeId.contains(entry.getKey()))
-        .forEach(entry -> removeScore(entry.getValue()));
-
+    ScoreSystemUtils.updateScoreSystemOfTalent(newScores, this, isAdmin);
     setFinalScore(obtainScore());
   }
 
