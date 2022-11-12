185c9058c0a10408e078866fa87e926be42b3ca5
Khương Minh Lữ <khuong.lu@adnovum.vn>
2022-11-12 22:30:30 +0700

    be-69: Refactor locationaddress - replace all LocationAddress json DTO to LocationAddress uid when create, update and LocationAddressDto when read - add basic data for location address - adjust mapper to align with this change

---
+++ src/main/resources/db/migration/V20221112_192233__DML_add_location_data.sql
@@ -0,0 +1,10 @@
+insert into location_address(id, city, district, street, coordinates, uid) VALUES
+	(1,'Ho Chi Minh', null, null, st_geomfromtext('POINT(106.660172 10.762622)'), gen_random_uuid());
+insert into location_address(id, city, district, street, coordinates, uid) VALUES
+	(2,'Ha Noi', null, null, st_geomfromtext('POINT(105.804817 21.028511)'), gen_random_uuid());
+insert into location_address(id, city, district, street, coordinates, uid) VALUES
+	(3,'Ho Chi Minh', 'Go Vap', null, st_geomfromtext('POINT(105.804817 21.028511)'), gen_random_uuid());
+insert into location_address(id, city, district, street, coordinates, uid) VALUES
+	(4,'Hồ Chí Minh', 'Phú Nhuận', null, st_geomfromtext('POINT(105.804817 21.028511)'), gen_random_uuid());
+insert into location_address(id, city, district, street, coordinates, uid) VALUES
+	(5,'Hồ Chí Minh', 'Tân Bình', 'Cộng Hoà', st_geomfromtext('POINT(105.804817 21.028511)'), gen_random_uuid());
