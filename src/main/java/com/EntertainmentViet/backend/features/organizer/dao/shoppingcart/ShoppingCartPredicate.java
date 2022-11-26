8765296666218a5f063a5e21332dc3ff9d08f648
Khương Minh Lữ <khuong.lu@adnovum.vn>
2022-11-26 22:58:45 +0700

    fix bug: add fetchJoin for shoppingcart

--- src/main/java/com/EntertainmentViet/backend/features/organizer/dao/shoppingcart/ShoppingCartPredicate.java
+++ src/main/java/com/EntertainmentViet/backend/features/organizer/dao/shoppingcart/ShoppingCartPredicate.java
@@ -1,5 +1,7 @@
 package com.EntertainmentViet.backend.features.organizer.dao.shoppingcart;
 
+import java.util.UUID;
+
 import com.EntertainmentViet.backend.domain.entities.booking.QJobDetail;
 import com.EntertainmentViet.backend.domain.entities.organizer.OrganizerShoppingCart;
 import com.EntertainmentViet.backend.domain.entities.organizer.QOrganizer;
@@ -8,6 +10,8 @@ import com.EntertainmentViet.backend.domain.entities.talent.QPackage;
 import com.EntertainmentViet.backend.domain.entities.talent.QTalent;
 import com.EntertainmentViet.backend.domain.standardTypes.WorkType;
 import com.EntertainmentViet.backend.domain.values.QCategory;
+import com.EntertainmentViet.backend.domain.values.QLocation;
+import com.EntertainmentViet.backend.domain.values.QLocationType;
 import com.EntertainmentViet.backend.features.common.dao.IdentifiablePredicate;
 import com.EntertainmentViet.backend.features.organizer.dto.shoppingcart.ListCartItemParamDto;
 import com.querydsl.core.types.ExpressionUtils;
@@ -16,8 +20,6 @@ import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-
-import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
@@ -31,7 +33,12 @@ public class ShoppingCartPredicate extends IdentifiablePredicate<OrganizerShoppi
   private final QTalent talent = QTalent.talent;
   private final QCategory category = QCategory.category;
   private final QCategory parentCategory = new QCategory("parent");
-
+  private final QLocation location = QLocation.location;
+  private final QLocationType locationType = QLocationType.locationType;
+  private final QLocation parentLocation = new QLocation("parent");
+  private final QLocationType parentLocationType = new QLocationType("parentLocationType");
+  private final QLocation grandparentLocation = new QLocation("grandparentLocation");
+  private final QLocationType grandParentLocationType = new QLocationType("grandParentLocationType");
 
   @Override
   public Predicate joinAll(JPAQueryFactory queryFactory) {
@@ -40,6 +47,12 @@ public class ShoppingCartPredicate extends IdentifiablePredicate<OrganizerShoppi
         .leftJoin(organizerShoppingCart.talentPackage, aPackage).fetchJoin()
         .leftJoin(aPackage.talent, talent).fetchJoin()
         .leftJoin(aPackage.jobDetail, jobDetail).fetchJoin()
+        .leftJoin(jobDetail.location, location).fetchJoin()
+        .leftJoin(location.type(), locationType).fetchJoin()
+        .leftJoin(location.parent(), parentLocation).fetchJoin()
+        .leftJoin(parentLocation.type(), parentLocationType).fetchJoin()
+        .leftJoin(parentLocation.parent(), grandparentLocation).fetchJoin()
+        .leftJoin(grandparentLocation.type(), grandParentLocationType).fetchJoin()
         .leftJoin(jobDetail.category, category).fetchJoin()
         .leftJoin(category.parent, parentCategory).fetchJoin()
         .fetch();
