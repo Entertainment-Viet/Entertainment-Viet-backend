54ca8d95053cb12a8aeb1657819b110cec93aec0
Hồ Huy Hoàng <48915815+hohuyhoangg@users.noreply.github.com>
2022-12-04 16:33:20 +0700

    add api admin bookings

---
+++ src/main/java/com/EntertainmentViet/backend/features/admin/boundary/bookings/AdminBookingBoundary.java
@@ -0,0 +1,19 @@
+package com.EntertainmentViet.backend.features.admin.boundary.bookings;
+
+import com.EntertainmentViet.backend.features.admin.dto.bookings.AdminListBookingParamDto;
+import com.EntertainmentViet.backend.features.booking.dto.booking.ListBookingResponseDto;
+import com.EntertainmentViet.backend.features.booking.dto.booking.ReadBookingDto;
+import com.EntertainmentViet.backend.features.booking.dto.booking.UpdateBookingDto;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface AdminBookingBoundary {
+
+    Optional<ReadBookingDto> findByUid(boolean isCurrentUser, UUID uid);
+
+    ListBookingResponseDto listBooking(boolean isCurrentUser, AdminListBookingParamDto paramDto, Pageable pageable);
+
+    Optional<UUID> update(UUID uid, UpdateBookingDto updateBookingDto);
+}
