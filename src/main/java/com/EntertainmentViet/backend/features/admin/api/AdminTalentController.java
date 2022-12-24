7bf01d5d41e290e42f2e5c09e5588127845369fd
Phat Luu <yadsendew@protonmail.com>
2022-12-24 10:46:09 +0700

    add endpoints for (dis)-approve talent

--- src/main/java/com/EntertainmentViet/backend/features/admin/api/AdminTalentController.java
+++ src/main/java/com/EntertainmentViet/backend/features/admin/api/AdminTalentController.java
@@ -36,20 +36,20 @@ public class AdminTalentController {
 
   @GetMapping(value = "/{uid}")
   public CompletableFuture<ResponseEntity<ReadAdminTalentDto>> findByUid(JwtAuthenticationToken token,
-                                                                         @PathVariable("admin_uid") UUID adminUid,
-                                                                         @PathVariable("uid") UUID uid) {
+      @PathVariable("admin_uid") UUID adminUid,
+      @PathVariable("uid") UUID uid) {
 
     if (!adminUid.equals(RestUtils.getUidFromToken(token)) && !RestUtils.isTokenContainPermissions(token, "ROOT")) {
-      log.warn(String.format("The token don't have enough access right to update information of admin with uid '%s'", adminUid));
+      log.warn(String.format("The token don't have enough access right to update information of admin with uid '%s'",
+          adminUid));
       return CompletableFuture.completedFuture(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
     }
 
     return CompletableFuture.completedFuture(adminTalentService.findByUid(adminUid, uid)
-        .map( talentDto -> ResponseEntity
+        .map(talentDto -> ResponseEntity
             .ok()
-            .body(talentDto)
-        )
-        .orElse( ResponseEntity.notFound().build()));
+            .body(talentDto))
+        .orElse(ResponseEntity.notFound().build()));
   }
 
   @PostMapping(value = "/{uid}")
@@ -66,25 +66,38 @@ public class AdminTalentController {
     return CompletableFuture.completedFuture(ResponseEntity.badRequest().build());
   }
 
-  @PutMapping(
-      consumes = MediaType.APPLICATION_JSON_VALUE,
-      produces = MediaType.APPLICATION_JSON_VALUE,
-      value = "/{uid}")
+  @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE, value = "/{uid}")
   public CompletableFuture<ResponseEntity<UUID>> update(JwtAuthenticationToken token,
-                                                                      @PathVariable("admin_uid") UUID adminUid,
-                                                                      @PathVariable("uid") UUID uid,
-                                                                      @RequestBody @Valid UpdateAdminTalentDto updateAdminTalentDto) {
+      @PathVariable("admin_uid") UUID adminUid,
+      @PathVariable("uid") UUID uid,
+      @RequestBody @Valid UpdateAdminTalentDto updateAdminTalentDto) {
 
     if (!adminUid.equals(RestUtils.getUidFromToken(token)) && !RestUtils.isTokenContainPermissions(token, "ROOT")) {
-      log.warn(String.format("The token don't have enough access right to update information of admin with uid '%s'", adminUid));
+      log.warn(String.format("The token don't have enough access right to update information of admin with uid '%s'",
+          adminUid));
       return CompletableFuture.completedFuture(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
     }
 
     return CompletableFuture.completedFuture(adminTalentService.update(updateAdminTalentDto, uid)
         .map(updatedTalent -> ResponseEntity
             .ok()
-            .body(updatedTalent)
-        ).orElse(ResponseEntity.badRequest().build())
-    );
+            .body(updatedTalent))
+        .orElse(ResponseEntity.badRequest().build()));
   }
+
+  @PostMapping(value = "/{uid}")
+   public CompletableFuture<ResponseEntity<Void>> approve(JwtAuthenticationToken token, @PathVariable("uid") UUID uid) {
+     if (adminTalentService.approve(uid)) {
+       return CompletableFuture.completedFuture(ResponseEntity.ok().build());
+     }
+     return CompletableFuture.completedFuture(ResponseEntity.badRequest().build());
+   }
+
+   @DeleteMapping(value = "/{uid}")
+   public CompletableFuture<ResponseEntity<Void>> disapprove(JwtAuthenticationToken token, @PathVariable("uid") UUID uid) {
+     if (adminTalentService.disapprove(uid)) {
+       return CompletableFuture.completedFuture(ResponseEntity.ok().build());
+     }
+     return CompletableFuture.completedFuture(ResponseEntity.badRequest().build());
+   }
 }
