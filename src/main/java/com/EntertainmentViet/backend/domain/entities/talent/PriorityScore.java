f9a70b60b6d79e86778833c2ef71bb82c0b47a38
duckhai1 <huynhduc.khai0@gmail..com>
2023-01-02 18:56:36 +0700

    Add on delete cascade for PriorityScore

--- src/main/java/com/EntertainmentViet/backend/domain/entities/talent/PriorityScore.java
+++ src/main/java/com/EntertainmentViet/backend/domain/entities/talent/PriorityScore.java
@@ -1,20 +1,13 @@
 package com.EntertainmentViet.backend.domain.entities.talent;
 
 import com.vladmihalcea.hibernate.type.array.ListArrayType;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
@@ -36,6 +29,7 @@ public class PriorityScore {
   private Talent talent;
 
   @ManyToOne(fetch = FetchType.LAZY)
+  @OnDelete(action = OnDeleteAction.CASCADE)
   @NotNull
   private ScoreType scoreType;
 
