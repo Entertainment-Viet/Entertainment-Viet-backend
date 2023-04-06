d566ecf77d42e7404d5df4909e631b564730c61d
duckhai1 <huynhduc.khai0@gmail..com>
2023-04-06 23:28:49 +0700

    Remove unused swagger properties expose

--- src/main/java/com/EntertainmentViet/backend/domain/values/RepeatPattern.java
+++ src/main/java/com/EntertainmentViet/backend/domain/values/RepeatPattern.java
@@ -1,6 +1,7 @@
 package com.EntertainmentViet.backend.domain.values;
 
 import com.EntertainmentViet.backend.domain.businessLogic.CronExpressionLogic;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import javax.persistence.Embeddable;
@@ -25,14 +26,17 @@ public class RepeatPattern {
 
   private OffsetDateTime endPeriod;
 
+  @Schema(hidden = true)
   public boolean isValid() {
     return cronExpression != null && startPeriod != null && endPeriod != null;
   }
 
+  @Schema(hidden = true)
   public List<OffsetDateTime> getAllOccurrence() {
     return CronExpressionLogic.getAllOccurrence(this);
   }
 
+  @Schema(hidden = true)
   public String getDescription() {
     return CronExpressionLogic.getDescription(this);
   }
